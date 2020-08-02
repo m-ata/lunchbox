1822bcc013e95c7ab089b6a52f27e500b20b3564
Muhammad Ata <Muhammad_ata@outlook.com>
2020-08-02 13:25:11 -0700

    working on menu detail screen

--- src/components/Home/MenuDetail.js
+++ src/components/Home/MenuDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View,   
         Text, 
@@ -7,12 +7,33 @@ import { View,
         ScrollView, 
         ImageBackground, 
         Image, 
-        Dimensions 
+        Dimensions,
+        Animated
     } from 'react-native';
 
 const MenuDetail = ({ route, navigation}) => {
 
     const { menu } = route.params;
+    const choices = [
+        {
+            name: 'Extra Spicy',
+            price: '2.00',
+            currency: 'SAR',
+            url: './../../../public/icons/radio.png'
+        },
+        {
+            name: 'No Extra Oil',
+            price: '2.00',
+            currency: 'SAR',
+            url: './../../../public/icons/radio.png'
+        },
+        {
+            name: 'Extra Half Rice',
+            price: '2.00',
+            currency: 'SAR',
+            url: './../../../public/icons/radio.png'
+        },
+    ]
 
     return(
         <View style={styles.container}>
@@ -30,7 +51,7 @@ const MenuDetail = ({ route, navigation}) => {
                     </View>
                 </ImageBackground>
             </View>
-            <View style={{height: 200, marginLeft: 16, marginTop: 16}}>
+            <View style={{minHeight: 100, marginLeft: 16, marginTop: 16}}>
                 <ScrollView keyboardShouldPersistTaps="handled">
                     <View style={{flexDirection:'row', flexWrap:'wrap'}}>
                         <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}> {menu.name} </Text>
@@ -39,6 +60,113 @@ const MenuDetail = ({ route, navigation}) => {
                     </View>
                     <Text style={{fontSize: 16, marginTop: 8}}> {menu.description} </Text>
                 </ScrollView>
+            </View>
+            <View style={{height: 250}}>
+                <View style={{flexDirection: 'row', 
+                                justifyContent: 'space-between', 
+                                backgroundColor: 'rgb(248, 248, 248)',
+                                minHeight: 50
+                                }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 16}}>
+                    <Text style={{color: 'black', fontSize: 18}}> Your Choices </Text>
+                    <Image 
+                        source={require('./../../../public/icons/arrow_up.png')}
+                        style={{marginLeft: 8}}
+                    />
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
+                        <TouchableOpacity 
+                            style={{
+                                backgroundColor: 'rgb(23, 179, 158)',
+                                borderRadius: 10,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                width: 100,
+                                height: 30,
+                                marginRight: 16
+                            }}
+                        >
+                            <Text style={{color: 'white',}}> REQUIRED </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <ScrollView>
+                    {
+                        choices.map(c => {
+                            return(
+                                <>
+                                    <View style={{flexDirection: 'row', justifyContent: 'space-between', minHeight: 50}} >
+                                        <View style={{ flexDirection: 'row', 
+                                                        justifyContent: 'flex-start', 
+                                                        alignItems: 'center', 
+                                                        marginLeft: 16}} >
+                                            <Image
+                                                source={require('./../../../public/icons/radio.png')}
+                                            />
+                                            <Text style={{ fontSize: 17, 
+                                                            fontWeight: 'bold', 
+                                                            color: 'black',
+                                                            marginLeft: 8
+                                                        }}> {c.name} </Text>
+                                        </View>
+                                        <View style={{ flexDirection: 'row', 
+                                                        justifyContent: 'flex-end',
+                                                        alignItems: 'center',
+                                                        marginRight: 16
+                                                        }}>
+                                            <Text>+ {c.price} {c.currency} </Text>
+                                        </View>
+                                    </View>
+                                    <View
+                                        style={{
+                                            borderBottomColor: 'rgb(248, 248, 248)',
+                                            borderBottomWidth: 2,
+                                        }}
+                                    />
+                                </>
+                            )
+                        })
+                    }
+                    <View style={{flexDirection: 'row', 
+                                justifyContent: 'space-between', 
+                                backgroundColor: 'rgb(248, 248, 248)',
+                                minHeight: 50
+                                }}>
+                                    <Text style={{color: 'black', fontSize: 18, marginLeft: 16}}> How many ? </Text>
+                    </View>
+                    <View style={{flexDirection: 'row', justifyContent: 'center', minHeight: 100}}>
+                        <Image
+                            source={require('./../../../public/icons/minus.png')}
+                            style={{marginTop: 16}}
+                        />
+                        <Text style={{marginLeft: 16, marginTop: 24, fontSize: 20, fontWeight: 'bold'}}>0</Text>
+                        <Image
+                            source={require('./../../../public/icons/plus.png')}
+                            style={{marginTop: 16, marginLeft: 16}}
+                        />
+                    </View>
+                </ScrollView>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <TouchableOpacity
+                    style={{borderRadius: 10, 
+                        backgroundColor: 'rgb(178, 178, 178)', 
+                        marginLeft: 16,
+                        marginRight: 16,
+                        padding: 10,
+                        width: Dimensions.get('window').width - 50,
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+                        <Text style={[styles.buttonText, { marginLeft: 16 }]}>ADD TO BASKET</Text>
+                        
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-start'}}>
+                        <Text style={[styles.buttonText, { marginRight: 16 }]}> 45.00 SAR</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -63,5 +191,10 @@ const styles = StyleSheet.create({
         bottom: 0,
         right: 0,
         left: Dimensions.get('window').width - 50
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 })
